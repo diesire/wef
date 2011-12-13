f31469fcd54b62b2a1de4bd069a3b057d63e0c11
Pablo Escalada Gómez <diesire@gmail.com>
2011-12-13 11:48:50 +0100

    Update code to pass tests

--- plugins/cssParser/test/cssParser.wefTest.js
+++ plugins/cssParser/test/cssParser.wefTest.js
@@ -9,11 +9,11 @@ TestCase("cssParser", {
         assertEquals("cssParser", wef.plugins.registered["cssParser"].name);
     },
     "test cssParser namespace":function () {
-        assertNotUndefined(wef.plugins.cssParser);
-        assertEquals("cssParser", wef.plugins.cssParser.name);
+        assertNotUndefined(wef.fn.cssParser);
+        assertEquals("cssParser", wef.fn.cssParser.name);
     },
     "test cssParser method":function () {
         var text = "body {display-model: \"a (intrinsic), b (intrinsic)\";} div#uno {situated: a; display-model: \"123 (intrinsic)\";}";
-        wef.plugins.registered.cssParser.cssParser().parse(text);
+        wef.plugins.registered.cssParser.init().parse(text);
     }
 });
