ff1e3c9691175adb40879b09ac57538ac17c45a9
Pablo Escalada Gómez <diesire@gmail.com>
2011-12-13 13:50:48 +0100

    Added initialization call test

--- test/wef.pluginsTest.js
+++ test/wef.pluginsTest.js
@@ -8,27 +8,24 @@ var correctPlugin = {
     version:"0.0.1",
     description:"An example plugin",
     init:function () {
-        return this.name;
+        return correctPlugin;
     }
-};
-
-var noInitPlugin = {
-    name:"noInitPlugin",
-    version:"0.0.1",
-    description:"An example plugin without init() method"
 };
 
 TestCase("wef.plugins", {
     "test plugins not null":function () {
         assertNotNull(wef.plugins);
     },
+
     "test plugin register":function () {
         wef.plugins.register("correctPlugin", correctPlugin);
         assertNotUndefined(wef.plugins.registered["correctPlugin"]);
     },
+
     "test plugin namespace":function () {
         assertNotUndefined(wef.fn.correctPlugin);
     },
+
     "test plugin already registered":function () {
         assertNotUndefined(wef.plugins.registered["correctPlugin"]);
         //not obvious
@@ -36,19 +33,44 @@ TestCase("wef.plugins", {
             wef.plugins.register("correctPlugin", correctPlugin)
         });
     },
+
     "test plugin remove":function () {
         wef.plugins.remove("correctPlugin");
         assertUndefined(wef.correctPlugin);
     },
+
     "test not found plugin remove":function () {
         assertUndefined(wef.fn.correctPlugin);
         assertException(function () {
             wef.plugins.remove("correctPlugin");
         });
     },
+
     "test no init() plugin":function () {
+        var noInitPlugin = {
+            name:"noInitPlugin",
+            version:"0.0.1",
+            description:"An example plugin without init() method"
+        };
         assertException(function () {
             wef.plugins.register("noInitPlugin", noInitPlugin);
         });
+    },
+
+    "test plugin initialization":function () {
+        var initPlugin = {
+            name:"initPlugin",
+            version:"0.0.1",
+            description:"An example plugin",
+            isInitialized:false,
+            init:function () {
+                this.isInitialized = true;
+                return correctPlugin;
+            }
+        };
+        assertFalse(initPlugin.isInitialized);
+        wef.plugins.register("initPlugin", initPlugin);
+        assertTrue(wef.fn.initPlugin.isInitialized);
+        assertTrue(initPlugin.isInitialized);
     }
 });
