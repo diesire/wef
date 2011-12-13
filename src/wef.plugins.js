1a545fd19fcccd9cbd6a9fbb50966a4c236fbef3
Pablo Escalada Gómez <diesire@gmail.com>
2011-12-13 11:45:53 +0100

    Refactor for better static parsing Added wef.fn namespace for installed plugins

--- src/wef.plugins.js
+++ src/wef.plugins.js
@@ -8,38 +8,41 @@
  * The wef plugins module.
  */
 (function () {
-    var plugins = {};
-    plugins.registered = {};
-    plugins.AlreadyRegisteredException = Error;
-    plugins.NotFoundException = Error;
+    var plugins = {
+        registered:{},
+        AlreadyRegisteredException:Error,
+        NotFoundException:Error,
 
-    /**
-     * Registers a plugin
-     * @param name Plugin name
-     * @param metadata The plugin
-     * @throws wef.plugins.AlreadyRegisteredException if a plugin with the same name has been registered
-     */
-    plugins.register = function (name, metadata) {
-        if (plugins.registered.hasOwnProperty(name)) {
-            throw new plugins.AlreadyRegisteredException('Plugin already registered: ' + name);
+        /**
+         * Registers a plugin
+         * @param name Plugin name
+         * @param metadata The plugin
+         * @throws wef.plugins.AlreadyRegisteredException if a plugin with the same name has been registered
+         */
+        register:function (name, metadata) {
+            if (plugins.registered.hasOwnProperty(name)) {
+                throw new plugins.AlreadyRegisteredException('Plugin already registered: ' + name);
+            }
+            plugins.registered[name] = metadata;
+            //TODO: register functions, events...
+        },
+
+        /**
+         * Removes a plugins
+         * @param name Plugin name
+         * @throws wef.plugins.NotFoundException if the plugin could not be found
+         */
+        remove:function (name) {
+            if (!plugins.registered.hasOwnProperty(name)) {
+                throw new plugins.NotFoundException('Plugin not found: ' + name);
+            } else {
+                delete plugins.registered[name];
+                //TODO: delete functions, events...
+            }
         }
-        plugins.registered[name] = metadata;
-        //TODO: register functions, events...
     };
 
-    /**
-     * Removes a plugins
-     * @param name Plugin name
-     * @throws wef.plugins.NotFoundException if the plugin could not be found
-     */
-    plugins.remove = function (name) {
-        if (!plugins.registered.hasOwnProperty(name)) {
-            throw new plugins.NotFoundException('Plugin not found: ' + name);
-        } else {
-            delete plugins.registered[name];
-            //TODO: delete functions, events...
-        }
-    };
-
+    //update namespaces
     wef.plugins = plugins;
+    wef.fn = plugins.registered; //shortcut, may change plugins.registered to private
 })();
