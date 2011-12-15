ca414edd549217cff610b026aab7a6fce5f38a08
Pablo Escalada Gómez <diesire@gmail.com>
2011-12-15 17:35:40 +0100

    Added StyleDeclaration Added additional data to PropertyFound event

--- plugins/cssParser/src/cssParser.wef.js
+++ plugins/cssParser/src/cssParser.wef.js
@@ -4,23 +4,23 @@
  * MIT Licensed
  */
 
-//requires: wef.events, console, cssParser
+//requires: wef.events, console, cssParser, wef
 //exports: cssParser
 
 /**
  * The cssParser plugin
  */
+
 (function () {
-    //TODO: CssParserAdapter
     var cssParser = {
         name:"cssParser",
         version:"0.0.1",
         description:"A CSS Parser. Wraps excellent JSCSSP library <http://glazman.org/JSCSSP/>",
         authors:["Pablo Escalada <uo1398@uniovi.es>"],
         licenses:["MIT"], //TODO: Licenses
-        events: {
-            PROPERTY_FOUND: "propertyFound",
-            CSSRULE_FOUND: "cssRuleFound"
+        events:{
+            PROPERTY_FOUND:"propertyFound",
+            CSSRULE_FOUND:"cssRuleFound"
         },
         init:function () {
             return cssParser;
@@ -33,14 +33,18 @@
                 //workaround. Not very glad of firing document events
                 var cssRuleEvent = document.createEvent("Event");
                 cssRuleEvent.initEvent(cssParser.events.CSSRULE_FOUND, true, true);
-                cssRuleEvent.cssRule = cssRule;
-
+                cssRuleEvent.cssRule = cssRule; //TODO: data exchange interface
                 document.dispatchEvent(cssRuleEvent);
 
                 cssRule.declarations.forEach(function (declaration) {
                     var propertyEvent = document.createEvent("Event");
                     propertyEvent.initEvent(cssParser.events.PROPERTY_FOUND, true, true);
-                    propertyEvent.declaration = declaration;
+
+                    var data = {
+                        selectorText:cssRule.selectorText,
+                        declaration:new StyleDeclaration(declaration.property, declaration.valueText)
+                    }
+                    propertyEvent.data = data;
                     document.dispatchEvent(propertyEvent);
                 });
             });
@@ -48,4 +52,19 @@
     };
 
     wef.plugins.register("cssParser", cssParser);
+
+    /**
+     * CSS Style declaration
+     * @param property property name
+     * @param valueText property value
+     */
+    function StyleDeclaration(property, valueText) {
+        this.property = property;
+        this.valueText = valueText;
+    };
+
+    StyleDeclaration.prototype = {
+        property:"",
+        valueText:""
+    };
 })();
