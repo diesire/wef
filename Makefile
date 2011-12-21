a5b617bb305d60183a49621039c424439c65f42e
Pablo Escalada Gómez <diesire@gmail.com>
2011-12-21 17:11:11 +0100

    Update to support log module

--- Makefile
+++ Makefile
@@ -1,4 +1,4 @@
-SRC = src/wef.core.js src/wef.plugins.js
+SRC = src/wef.core.js src/wef.plugins.js src/wef.log.js
 SRC_PARSER = plugins/cssParser/src/cssParser.wef.js plugins/cssParser/lib/cssParser.js
 SRC_TEMPLATE = plugins/templateLayout/src/templateLayout.wef.js
 
--- build/wef.js
+++ build/wef.js
@@ -71,3 +71,38 @@
     wef.plugins = plugins;
     wef.fn = plugins.registered;
 })();
+/*!
+ * Wef logger
+ * Copyright (c) 2011 Pablo Escalada
+ * MIT Licensed
+ */
+
+/**
+ * The wef plugins module.
+ */
+(function () {
+    var backend = console;
+    var debug = function (message) {
+        backend.debug(message);
+    };
+    var error = function (message) {
+        backend.error(message);
+    };
+    var info = function (message) {
+        backend.info(message);
+    };
+    var warn = function (message) {
+        backend.warn(message);
+    };
+    var log = {
+        debug:debug,
+        info:info,
+        warn:warn,
+        error:error
+    };
+    log.log = info;
+
+
+    //update namespaces
+    wef.log = log;
+})();
