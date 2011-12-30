3cb034e7e968f879d5c4290450c357a5e2663c3b
Pablo Escalada Gómez <diesire@gmail.com>
2011-12-30 19:07:48 +0100

    Add qunit test

---
+++ test/wef.coreTest-qunit.js
@@ -0,0 +1,21 @@
+/*!
+ * wef.core tests
+ * Copyright (c) 2011 Pablo Escalada
+ * MIT Licensed
+ */
+test("namespace", function() {
+    notEqual(wef, undefined, "is wef namespace defined?");
+    equal(typeof wef, "function", "is wef a function?");
+});
+
+test("wef.fn", function() {
+    notEqual(typeof wef.fn, undefined, "is wef.fn defined ?");
+});
+
+test("constructor", function() {
+    equal(typeof wef(), "object", "empty constructor returns an object")
+});
+
+test("public properties", function() {
+    equal(typeof wef.fn.version, "string", "wef().version");
+});
