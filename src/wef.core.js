a36ff58130d49a90e1c29983dd1532dc795f86fb
Pablo Escalada Gómez <diesire@gmail.com>
2012-01-02 23:30:31 +0100

    Change wef type "function" -> "object"

--- src/wef.core.js
+++ src/wef.core.js
@@ -8,31 +8,41 @@
  * wef module
  */
 (function(global) {
-    var wef = {
+    var wef = function() {
+        return new wef.prototype.init();
+    };
+    wef.prototype = {
+        constructor:wef,
         version: "0.0.1",
-        extend: function (receiver, giver) {
-            var tmp = receiver;
-            //both must be objects
-            if (typeof receiver == typeof giver == "object") {
-                if (tmp == null) {
-                    tmp = {};
-                }
-                if (receiver == null) {
-                    return tmp;
-                }
-                for (var property in giver) {
-                    tmp.property = giver.property;
-                }
-                return tmp
-            }
-            throw new Error("InvalidArgumentException: incorrect argument type");
+        init: function() {
+            return this;
         }
+    };
+    wef.fn = wef.prototype;
+    wef.prototype.init.prototype = wef.prototype;
+
+    wef.fn.extend = function (receiver, giver) {
+        var tmp = receiver;
+        //both must be objects
+        if (typeof receiver === "object" && typeof giver === "object") {
+            if (tmp == null) {
+                tmp = {};
+            }
+            if (receiver == null) {
+                return tmp;
+            }
+            for (var property in giver) {
+                tmp[property] = giver[property];
+            }
+            return tmp
+        }
+        throw new Error("InvalidArgumentException: incorrect argument type");
     };
 
     //registering global variable
     if (global.wef) {
         throw new Error("wef has already been defined");
     } else {
-        global.wef = wef;
+        global.wef = wef();
     }
 })(window);
