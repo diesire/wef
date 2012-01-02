173ef9f80ca7a96b6fa8f3435b9c3c53c97466d0
Pablo Escalada Gómez <diesire@gmail.com>
2012-01-02 19:05:54 +0100

    Add wef.extend(). Only handle plain objects

--- src/wef.core.js
+++ src/wef.core.js
@@ -9,11 +9,28 @@
  */
 (function(global) {
     var wef = {
-        version: "0.0.1"
+        version: "0.0.1",
+        extend: function (receiver, giver) {
+            var tmp = receiver;
+            //both must be objects
+            if (typeof receiver == typeof giver == "object") {
+                if (tmp == null) {
+                    tmp = {};
+                }
+                if (receiver == null) {
+                    return tmp;
+                }
+                for (var property in giver) {
+                    tmp.property = giver.property;
+                }
+                return tmp
+            }
+            throw new Error("InvalidArgumentException: incorrect argument type");
+        }
     };
 
     //registering global variable
-    if(global.wef) {
+    if (global.wef) {
         throw new Error("wef has already been defined");
     } else {
         global.wef = wef;
