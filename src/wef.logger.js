3d810ad514904a8dc34d82d798e0a571d463faf2
Pablo Escalada Gómez <diesire@gmail.com>
2012-01-09 11:15:06 +0100

    Revert 8a4a164b0d8b272652b32e9ce63e15c741f5069f^..HEAD

--- src/wef.logger.js
+++ src/wef.logger.js
@@ -1,9 +1,11 @@
+//requires: wef
+
 /*!
  * Wef logger
  * Copyright (c) 2011 Pablo Escalada
  * MIT Licensed
  */
-(function (wef) {
+(function () {
     var LOGLEVEL = {
         all:-1,
         trace:1,
@@ -13,58 +15,57 @@
         warn:4,
         error:5,
         none:100
-    }, textFormatter, registered = {}, lastLogger, failSafeIndentation = false, logger, filteredLogs = 0;
+    };
 
-    textFormatter = function () {
+    var textFormatter = function() {
         return this;
     };
 
     textFormatter.prototype.format = function (messages, indentationLevel, type) {
-        var tmp = [], levelMarks = "                                                                                            ", levelText, typeText;
-        tmp = new Array(Array.prototype.slice.call(messages, tmp));
+        var tmp = [],
+            levelMarks = "                                                                                            ",
+            levelText, typeText;
+        tmp = Array.prototype.slice.call(messages, tmp);
         if (failSafeIndentation && indentationLevel) {
             levelText = levelMarks.slice(0, indentationLevel);
             tmp.unshift(levelText);
         }
         if (type) {
             typeText = "[" + type + "]";
-            tmp.unshift(typeText);
+            tmp.unshift(levelText);
         }
         return tmp;
     };
 
-    logger = function (logName) {
-        if (!logName || logName === "") {
-            logName = "default";
-        }
-        lastLogger = logName;
-        if (registered[lastLogger]) {
-            return registered[lastLogger].logger;
-        } else {
-            var tmpLogger = new logger.prototype.init(lastLogger);
-            registered[lastLogger] = {logLevel:LOGLEVEL.all, indentationLevel:0, logger:tmpLogger};
-            return tmpLogger;
-        }
-    };
+    var registered = {},
+        lastLogger,
+        failSafeIndentation = false,
+        logger = function(logName) {
+            if (!logName || logName == "") {
+                logName = "default";
+            }
+            var name = lastLogger = logName || lastLogger;
+            if (registered[lastLogger]) {
+                return registered[lastLogger].logger;
+            } else {
+                var tmpLogger = new logger.prototype.init(lastLogger);
+                registered[lastLogger] = {logLevel: LOGLEVEL.all, indentationLevel: 0, logger: tmpLogger};
+                return tmpLogger;
+            }
+        };
 
     logger.prototype.constructor = logger;
-
     logger.prototype.version = "0.0.1";
-
-    logger.prototype.logName = "";
-
     logger.prototype.formatter = new textFormatter();
-
     logger.prototype.init = function (logName) {
         this.logName = logName;
         return this;
     };
 
-    logger.prototype._filteredLogs = function () {
+    logger.prototype._filteredLogs = function() {
         return filteredLogs;
     };
-
-    logger.prototype._getIndentLevel = function (logName) {
+    logger.prototype._getIndentLevel = function(logName) {
         return registered[logName].indentationLevel;
     };
 
@@ -80,31 +81,27 @@
      * compatible.
      * */
     logger.filter = logger.prototype.filter = function (options) {
-        if (!options) {
-            return this;
-        }
+        if (!options) return this;
 
         if (options == "none" || options == "off") {
-            options = {logLevel:LOGLEVEL.none, pattern:".*"};
+            options = {logLevel: LOGLEVEL.none, pattern: ".*"};
         }
 
         if (options == "all" || options == "on") {
-            options = {logLevel:LOGLEVEL.all, pattern:".*"};
+            options = {logLevel: LOGLEVEL.all, pattern: ".*"};
         }
 
         if (!options.logLevel || typeof options.logLevel != "number" || !options.pattern || typeof options.pattern != "string") {
             //do nothing
             return this;
         }
-        var regExp = new RegExp(options.pattern), logLevel = options.logLevel, name;
+        var regExp = new RegExp(options.pattern), logLevel = options.logLevel;
 
-        for (name in registered) {
-            if (registered.hasOwnProperty(name)) {
-                if (regExp.test(name)) {
-                    registered[name].logLevel = logLevel;
-                } else {
-                    registered[name].logLevel = LOGLEVEL.none;
-                }
+        for (var name in registered) {
+            if (regExp.test(name)) {
+                registered[name].logLevel = logLevel;
+            } else {
+                registered[name].logLevel = LOGLEVEL.none;
             }
         }
         filteredLogs = 0;
@@ -112,35 +109,23 @@
     };
 
     logger.prototype.backend = window.console || {};
-
     logger.prototype.backend.failSafe = function () {
         //silent
     };
-
     logger.prototype.backend.failSafeGroup = function () {
         failSafeIndentation = true;
     };
-
     logger.prototype.backend.failSafeGroupEnd = function () {
         failSafeIndentation = true;
     };
-
     logger.prototype.backend.trace = window.console.trace || logger.prototype.backend.log;
-
     logger.prototype.backend.log = window.console.log || logger.prototype.backend.failSafe;
-
     logger.prototype.backend.debug = window.console.debug || logger.prototype.backend.log;
-
     logger.prototype.backend.info = window.console.info || logger.prototype.backend.log;
-
     logger.prototype.backend.warn = window.console.warn || logger.prototype.backend.log;
-
     logger.prototype.backend.error = window.console.error || logger.prototype.backend.log;
-
     logger.prototype.backend.group = window.console.group || logger.prototype.backend.failSafeGroup;
-
     logger.prototype.backend.groupCollapsed = window.console.groupCollapsed || window.console.group || logger.prototype.backend.failSafeGroup;
-
     logger.prototype.backend.groupEnd = window.console.groupEnd || logger.prototype.backend.failSafeGroupEnd;
 
     logger.prototype.init.prototype = logger.prototype;
@@ -226,5 +211,4 @@
     };
 
     wef.logger = logger;
-    
-})(window.wef);
+})();
--- test/wef.cssParserTest.js
+++ test/wef.cssParserTest.js
@@ -4,45 +4,87 @@
  * MIT Licensed
  */
 module("cssParser");
-test("namespace", function() {
-    notEqual(wef.cssParser , undefined, "is wef.cssParser namespace defined?");
+test("namespace", function () {
+    notEqual(wef.cssParser, undefined, "is wef.cssParser namespace defined?");
     equal(typeof wef.cssParser, "function", "is wef.cssParser a function?");
 });
 
-test("constructor", function() {
+test("constructor", function () {
     equal(typeof wef.cssParser(), "object", "empty constructor returns an object");
 });
 
-test("public properties", function() {
+test("public properties", function () {
     equal(typeof wef.cssParser().version, "string", "wef.cssParser().version");
 });
 
-test("pubic methods", function() {
+test("pubic methods", function () {
     var text = 'body { height: 100%; display: "a.b.c"  /2em "....."  /1em "d.e.f" "....."  /1em "g.h.i"  /2em 5em 1em  *  1em 10em}';
     notEqual(wef.cssParser().parse(text), null, "parse(string) ok");
 });
 
-test("parse exceptions", function() {
-    raises(function() {
+test("parse exceptions", function () {
+    raises(function () {
         wef.cssParser().parse("asd");
     }, "invalid css data throws an exception");
-    raises(function() {
+    raises(function () {
         wef.cssParser().parse(444);
     }, "parse(number) throws an exception");
-    raises(function() {
-        wef.cssParser().parse()
+    raises(function () {
+        wef.cssParser().parse();
     }, "parse(undefined) throws an exception");
-    raises(function() {
-        wef.cssParser().parse(null)
+    raises(function () {
+        wef.cssParser().parse(null);
     }, "parse(null) throws an exception");
-    raises(function() {
-        wef.cssParser().parse("")
+    raises(function () {
+        wef.cssParser().parse("");
     }, "empty data throws an exception");
 
     notEqual(wef.cssParser().parse("body{}"), null, "empty cssRule ok");
     notEqual(wef.cssParser().parse("body{234}"), null, "invalid cssRule declaration ok");
     notEqual(wef.cssParser().parse("body{} h1{display:none}"), null, "empty cssRule ok");
 
-    notEqual(wef.cssParser().whenError(function() {}).parse("asd"), null, "error callback catches exception");
-    notEqual(wef.cssParser().whenError(function() {}).parse("asd").parse("body{}"), null, "error callback allows chaining");
+    notEqual(wef.cssParser().whenError(
+        function () {
+        }).parse("asd"), null, "error callback catches exception");
+    notEqual(wef.cssParser().whenError(
+        function () {
+        }).parse("asd").parse("body{}"), null, "error callback allows chaining");
 });
+
+//test("events", function () {
+//    var text = "body {display: \"a\"}", events = [];
+//
+//    stop();
+//    wef.cssParser().whenStart(function (e) {
+//        console.log("start");
+//        events.push(e.type);
+//    });
+//    wef.cssParser().whenProperty(function (e) {
+//        console.log("prop");
+//
+//        events.push(e.type);
+//    });
+//    wef.cssParser().whenCssRule(function (e) {
+//        console.log("css");
+//
+//        events.push(e.type);
+//    });
+//    wef.cssParser().whenStop(function (e) {
+//        console.log("stop");
+//
+//        events.push(e.type);
+//    });
+//    wef.cssParser().parse(text);
+//
+//    setTimeout(function () {
+//        //expect(5);
+//
+//
+//        //equal(4, events.length);
+//        //equal("parserStart", events.shift());
+//        //equal("cssRuleFound", events.shift());
+//        //equal("propertyFound", events.shift());
+//        //equal("parserDone", events.shift());
+//        start();
+//    }, 1000);
+//});
