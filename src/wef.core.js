2c16565e676c7dac527eaf97e29b100faf20fb2c
Pablo Escalada Gómez <diesire@gmail.com>
2012-01-09 11:51:09 +0100

    Fix module call and refactor

--- src/wef.core.js
+++ src/wef.core.js
@@ -7,22 +7,25 @@
 /**
  * wef module
  */
-(function(global) {
-    var wef = function() {
+(function () {
+    var wef = function () {
         return new wef.prototype.init();
     };
+
     wef.prototype = {
         constructor:wef,
-        version: "0.0.1",
-        init: function() {
+        version:"0.0.1",
+        init:function () {
             return this;
         }
     };
+
     wef.fn = wef.prototype;
+
     wef.prototype.init.prototype = wef.prototype;
 
     wef.fn.extend = function (receiver, giver) {
-        var tmp = receiver;
+        var tmp = receiver, property;
         //both must be objects
         if (typeof receiver === "object" && typeof giver === "object") {
             if (tmp === null) {
@@ -31,7 +34,7 @@
             if (receiver === null) {
                 return tmp;
             }
-            for (var property in giver) {
+            for (property in giver) {
                 tmp[property] = giver[property];
             }
             return tmp;
@@ -49,13 +52,14 @@
     };
 
     wef.fn.error = function (message) {
-        throw new Error(message);   
+        throw new Error(message);
     };
 
     //registering global variable
-    if (global.wef) {
+    if (window.wef) {
         throw new Error("wef has already been defined");
     } else {
-        global.wef = wef();
+        window.wef = wef();
     }
-})(window);
+
+})();
